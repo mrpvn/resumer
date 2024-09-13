634eabd99b3ed387e8c8a9361315577a3353e893
mrpvn <pvnsingh05@gmail.com>
2024-09-13 23:22:09 +0530

    set light theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <TanstackProvider>
-            <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} disableTransitionOnChange>
               <Header/>
               {children}
               <Toaster />
